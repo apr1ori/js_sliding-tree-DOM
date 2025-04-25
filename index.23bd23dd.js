a62706573fd05b60eb09f4238b72d330a1f9f061
apr1ori <vladgolenok4@gmail.com>
2025-04-25 08:58:51 +0200

    make build

---
+++ index.23bd23dd.js
@@ -0,0 +1,2 @@
+document.querySelector("ul").addEventListener("click",function(e){var t=e.target.closest("li");if(t){var l=t.querySelector("ul");l&&l.classList.toggle("hide")}});
+//# sourceMappingURL=index.23bd23dd.js.map
